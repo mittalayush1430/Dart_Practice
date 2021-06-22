a70e98c0cc7d28a645600148d55f8184610d4a19
mittalayush1430 <mittalayush1430@gmail.com>
2021-06-23 03:08:41 +0530

    method to find common elements between two lists

--- loops.dart
+++ loops.dart
@@ -1,2 +1,21 @@
-List first = [4, 7, 1, 9, 5, 3, 6];
-List second = [9, 5, 3, 6, 1, 2, 8];
+List<int> first = [4, 7, 1, 9, 5, 3, 6];
+List<int> second = [9, 5, 3, 6, 1, 2, 8];
+
+//method to find the common elements in the two lists provided
+
+List<int> loopTester() {
+  List<int> commonNumbers = [];
+  for (int num1 in first) {
+    for (int num2 in second) {
+      if (num1 == num2) {
+        commonNumbers.add(num1);
+      }
+    }
+  }
+  print(commonNumbers);
+  return commonNumbers;
+}
+
+void main() {
+  loopTester();
+}
