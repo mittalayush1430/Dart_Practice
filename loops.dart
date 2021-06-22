ca876a77ea496b4d6ec20ce9a899d46200b56c39
mittalayush1430 <mittalayush1430@gmail.com>
2021-06-23 03:10:28 +0530

    added important comments in the code

--- loops.dart
+++ loops.dart
@@ -1,7 +1,9 @@
+// two sample lists
+
 List<int> first = [4, 7, 1, 9, 5, 3, 6];
 List<int> second = [9, 5, 3, 6, 1, 2, 8];
 
-//method to find the common elements in the two lists provided
+//method to find common elements in the two lists given
 
 List<int> loopTester() {
   List<int> commonNumbers = [];
